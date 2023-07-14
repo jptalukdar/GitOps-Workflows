2e22909544e37b3e60ced9a8ab4810e4a0ce87ae
Jyotiplaban Talukdar <36496193+jptalukdar@users.noreply.github.com>
2023-07-14 10:45:19 +0530

    Updated workflow

--- src/invoke.py
+++ src/invoke.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class Deployment():
   base_path = ""
   base_name = ""
+  subfiles = []
   config = {}
   def set_base_name(self, name):
     self.base_name = name
@@ -12,6 +13,13 @@ class Deployment():
   def deploy(self):
     print(f"Processing {self.base_name}")
     self.mark_completed()
+
+  def delete(self):
+    print(f"Deleting {self.base_name}")
+    for file in self.subfiles:
+      os.remove(os.path.join(self.base_path, file))
+    os.rmdir(self.base_path)
+
   
   def mark_completed(self):
     with open(os.path.join(self.base_path,"completed.keep") , "w") as fp:
@@ -35,8 +43,9 @@ def read_json(path: str) -> dict:
 
 def get_deployments(root_dir):
   deployments = []
+  deletions = []
   for root, dirs, files in os.walk(root_dir):
-    print("Found deployments: ",dirs, " on ", root_dir)
+    print("Found deployments: ",dirs, " on ", root_dir, files)
     for dir in dirs:
       print("Parsing Directory: " + os.path.join(root, dir))
       d_ = Deployment()
@@ -44,20 +53,32 @@ def get_deployments(root_dir):
       d_.set_base_name(os.path.basename(dir))
       subfiles = os.listdir(d_.base_path)
       subfiles = [f for f in subfiles if os.path.isfile(os.path.join(d_.base_path, f))] #Filtering only the files.
+      d_.subfiles = subfiles
+      if "delete.keep" in subfiles:
+        deletions.append(d_)
+        continue
       if "completed.keep" in subfiles:
         continue # Directory allready processed so skip 
       if "config.json" in subfiles:
         d_.config = read_json(os.path.join(d_.base_path, "config.json"))
       deployments.append(d_)
+    break
 
-  return deployments
+  return deployments,deletions
 
-def process_deployment(deploy_list: list[Deployment]) -> None:
+def process_deployment_creation(deploy_list: list[Deployment]) -> None:
   for d in deploy_list:
     print(f"Deploying {d.base_name}")
     d.deploy()
     mark_changed()
 
+def process_deployment_deletion(deploy_list: list[Deployment]) -> None:
+  for d in deploy_list:
+    print(f"Deleting {d.base_name}")
+    d.delete()
+    mark_changed()
+
 if __name__ == "__main__":
   d_list = get_deployments("deployments")
-  process_deployment(d_list)
+  process_deployment_creation(d_list[0])
+  process_deployment_deletion(d_list[1])
